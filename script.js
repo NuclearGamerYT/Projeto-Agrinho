8f363a34c731044155c9ba334b33ffc24a155295
NuclearGamerYT <166153016+NuclearGamerYT@users.noreply.github.com>
2026-06-02 22:03:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function responder(num) {
+
+    if (num === 1) {
+        document.getElementById("resposta1").innerHTML = 
+        "É um tipo de produção que respeita o meio ambiente e usa menos recursos naturais."
+    }
+
+    if (num === 2) {
+        document.getElementById("resposta2").innerHTML = 
+        "Práticas não sustentáveis causam poluição, desmatamento e prejudicam o solo e a água. Também afetam a saúde humana e contribuem para o aquecimento global."
+    }
+}
